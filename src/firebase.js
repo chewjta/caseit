a098866c499f70e3bc067907042ebcbfa5f96b33
Alvis <chewjta@gmail.com>
2020-11-16 17:57:11 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZP80dW9foi5znzbV-E4xy_M8hlnkHz4A",
-  authDomain: "ecomm-23f2e.firebaseapp.com",
-  databaseURL: "https://ecomm-23f2e.firebaseio.com",
-  projectId: "ecomm-23f2e",
-  storageBucket: "ecomm-23f2e.appspot.com",
-  messagingSenderId: "944197233555",
-  appId: "1:944197233555:web:883ddf9180e771aebd9666",
+
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
